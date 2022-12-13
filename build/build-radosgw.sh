326b986982c9183e6accf76f7fd1203012718486
Moritz Röhrich <moritz.rohrich@suse.com>
2022-12-13 16:01:19 +0100

    ci: fix build-radosgw script and linter
    - Fix reading markdownlint configuration
    - Fix build process by going back to Ninja instead of GnuMake, since the
      Makefiles generated by CMake seem bugged with random compile errors on
      first run and inconsistent targets
    
    Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

--- build/build-radosgw.sh
+++ build/build-radosgw.sh
@@ -12,9 +12,12 @@ CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-"Debug"}
 CEPH_DIR=$(realpath "${CEPH_DIR:-"/srv/ceph"}")
 S3GW_CCACHE_DIR=${S3GW_CCACHE_DIR:-"${CEPH_DIR}/build.ccache"}
 S3GW_BUILD_DIR="${CEPH_DIR}/build"
+S3GW_STATIC=${S3GW_STATIC:-"ON"}
 
+WITH_CEPHFS=${WITH_CEPHFS:-"OFF"}
 WITH_JAEGER=${WITH_JAEGER:-"OFF"}
 WITH_LTTNG=${WITH_LTTNG:-"OFF"}
+WITH_BABELTRACE=${WITH_BABELTRACE:-"OFF"}
 WITH_MANPAGE=${WITH_MANPAGE:-"OFF"}
 WITH_OPENLDAP=${WITH_OPENLDAP:-"OFF"}
 WITH_RADOSGW_AMQP_ENDPOINT=${WITH_RADOSGW_AMQP_ENDPOINT:-"OFF"}
@@ -24,12 +27,23 @@ WITH_RADOSGW_LUA_PACKAGES=${WITH_RADOSGW_LUA_PACKAGES:-"OFF"}
 WITH_RADOSGW_MOTR=${WITH_RADOSGW_MOTR:-"OFF"}
 WITH_RADOSGW_SELECT_PARQUET=${WITH_RADOSGW_SELECT_PARQUET:-"OFF"}
 WITH_RDMA=${WITH_RDMA:-"OFF"}
-WITH_SYSTEM_BOOST=${WITH_SYSTEM_BOOST:-"ON"}
+WITH_SYSTEM_BOOST=${WITH_SYSTEM_BOOST:-"OFF"}
 WITH_TESTS=${WITH_TESTS:-"OFF"}
+
+if [ "${S3GW_STATIC}" == "ON" ] ; then
+  ENABLE_SHARED=${ENABLE_SHARED:-"OFF"}
+  CEPH_SHARED=${CEPH_SHARED:-"STATIC"}  # STATIC | SHARED | MODULE
+  WITH_STATIC_LIBSTDCXX=${WITH_STATIC_LIBSTDCXX:-"ON"}
+else
+  ENABLE_SHARED="ON"
+  CEPH_SHARED="SHARED"
+  WITH_STATIC_LIBSTDCXX="OFF"
+fi
 
 NPROC=${NPROC:-$(nproc --ignore=2)}
 
 CEPH_CMAKE_ARGS=(
+  "-GNinja"
   "-DWITH_CCACHE=ON"
   "-DBOOST_J=${NPROC}"
   "-DCMAKE_C_COMPILER=gcc-11"
@@ -37,8 +51,17 @@ CEPH_CMAKE_ARGS=(
   "-DENABLE_GIT_VERSION=ON"
   "-DWITH_PYTHON3=3"
   "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+  "-DENABLE_SHARED=${ENABLE_SHARED}"
+  "-DWITH_STATIC_LIBSTDCXX=${WITH_STATIC_LIBSTDCXX}"
+  "-DCEPH_SHARED=${CEPH_SHARED}"
+  "-DWITH_CEPHFS=${WITH_CEPHFS}"
+  "-DWITH_LIBCEPHFS=OFF"
+  "-DWITH_LIBCEPHSQLITE=OFF"
+  "-DWITH_LIBRADOSSTRIPER=OFF"
+  "-DWITH_LIBURING=OFF"
   "-DWITH_JAEGER=${WITH_JAEGER}"
   "-DWITH_LTTNG=${WITH_LTTNG}"
+  "-DWITH_BABELTRACE=${WITH_BABELTRACE}"
   "-DWITH_MANPAGE=${WITH_MANPAGE}"
   "-DWITH_OPENLDAP=${WITH_OPENLDAP}"
   "-DWITH_RADOSGW_AMQP_ENDPOINT=${WITH_RADOSGW_AMQP_ENDPOINT}"
@@ -93,17 +116,17 @@ _configure() {
 _build() {
   cd "${S3GW_BUILD_DIR}"
 
-  make -j"${NPROC}" radosgw
+  ninja -j"${NPROC}" bin/radosgw
 
   if [ "${WITH_TESTS}" == "ON" ] ; then
-    make -j"${NPROC}" \
-      unittest_rgw_sfs_sqlite_users \
-      unittest_rgw_sfs_sqlite_buckets \
-      unittest_rgw_sfs_sqlite_objects \
-      unittest_rgw_sfs_sqlite_versioned_objects \
-      unittest_rgw_sfs_sfs_bucket \
-      unittest_rgw_sfs_metadata_compatibility \
-      unittest_rgw_sfs_gc
+    ninja -j"${NPROC}" \
+      bin/unittest_rgw_sfs_sqlite_users \
+      bin/unittest_rgw_sfs_sqlite_buckets \
+      bin/unittest_rgw_sfs_sqlite_objects \
+      bin/unittest_rgw_sfs_sqlite_versioned_objects \
+      bin/unittest_rgw_sfs_sfs_bucket \
+      bin/unittest_rgw_sfs_metadata_compatibility \
+      bin/unittest_rgw_sfs_gc
   fi
 }
 
